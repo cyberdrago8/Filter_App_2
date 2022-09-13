84d968325f4a08d733067a5eef3ba599fa065c4e
cyberdrago8 <53197677+cyberdrago8@users.noreply.github.com>
2022-09-13 15:48:25 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function preload () {
+}
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet is Inialized');
+}
+
+function gotPoses(results)
